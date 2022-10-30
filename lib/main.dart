55dc6b788db38f3c374b8051a229fcaa40b239fe
Alberto Pinto <albertomoya007@gmail.com>
2022-10-30 22:09:11 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/view/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hola',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MainScreen(),
+        '/main': (context) => const MainScreen(),
+      },
+    );
+  }
+}
---
+++ lib/view/main_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.red,
+      body: Center(child: Text('hola')),
+    );
+  }
+}
